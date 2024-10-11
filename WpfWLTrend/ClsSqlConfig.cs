649692c8d931c37bade5e3d13ea4037a3f742a7e
Porntep Kulapatna <pkulapatna@forte-tec.com>
2024-10-11 12:59:11 -0400

    Add project files.

---
+++ WpfWLTrend/ClsSqlConfig.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using WpfWLTrend.Properties;
+
+namespace WpfWLTrend
+{
+    public class ClsSqlConfig
+    {
+        public SqlConnection SqlConnection { get; set; }
+        
+        public string m_CurrentWLTable { get; set; }
+        public string m_PreviousWLTable { get; set; }
+
+        public string DataSource { get; set; }
+
+        //Work Stations
+        public string LocalWorkStationID { get; set; }
+
+        public string TargetWorkStationID { get; set; }
+        //Connection strings
+        public string m_ConString { get; set; }
+        public string m_WLConStr { get; set; }
+        public string m_MasterWLConStr { get; set; }
+        public string strRemoteMasterCon { get; set; }
+        private string m_strMasterCon { get; set; }
+        public string m_strUserName { get; set; }
+        public string m_strPassWrd { get; set; }
+        public string m_strDatabase { get; set; }
+        public string m_strWLDatabase { get; set; }
+
+        public string m_strHostID { get; set; }
+        public string m_strInstance { get; set; }
+
+        public const int BALE_ARCHIVE = 0;
+        public const int LOT_ARCHIVE = 1;
+        public const int UNIT_ARCHIVE = 2;
+        public const int WET_LAYER = 3;
+        public const int QULITY_ARCHIVE = 4;
+
+        public List<string> WLTableList;
+
+        public List<string> m_LineList { get; set; }
+        public List<string> m_SourceList { get; set; }
+
+
+        public ClsSqlConfig()
+        {
+            WLTableList = new List<string>();
+            SetSqlParams();
+            SetConnectionString();
+
+        }
+
+        internal List<string> GetAllWLTables()
+        {
+            WLTableList = GettableList(WET_LAYER);
+            return WLTableList;
+        }
+
+        private List<string> GettableList(int wET_LAYER)
+        {
+            List<string> tablelist = new List<string>();
+            string strquery = "SELECT [name],create_date FROM sys.tables WHERE [name] LIKE '%FValueReadings%' ORDER BY create_date DESC";
+            
+            try
+            {
+                using (var sqlConnection = new SqlConnection(m_MasterWLConStr))
+                {
+                    if (sqlConnection != null) sqlConnection.Open();
+                    using (var command = new SqlCommand(strquery, sqlConnection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                                while (reader.Read())
+                                {
+                                    if (reader.GetString(0) != null)
+                                        tablelist.Add(reader.GetString(0));
+                                }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in GettableList -> " + ex.Message);  
+            }
+            return tablelist;
+        }
+
+        internal List<string> GetUniqueStrItemlist(string strItem, string strTable)
+        {
+
+            string constr = m_ConString;
+            List<string> itemList = new List<string>();
+            string strQuery = "SELECT DISTINCT " + strItem + " From " + strTable + " ORDER BY " + strItem + ";";
+
+            if (strItem == "BalerID") constr = m_MasterWLConStr;
+
+            try
+            {
+                using (var sqlConnection = new SqlConnection(constr))
+                {
+                    if (sqlConnection != null) sqlConnection.Open();
+                    using (var command = new SqlCommand(strQuery, sqlConnection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                                while (reader.Read())
+                                {
+                                    if (reader != null)
+                                        itemList.Add(reader[0].ToString());
+                                }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in GetUniqueStrItemlist " + ex.Message);
+            }
+            return itemList;
+        }
+
+        internal DataTable GetNewWLDataTable(object selectedValue, string strQuery)
+        {
+            DataTable mytable = new DataTable();
+
+            try
+            {
+                using (var sqlConnection = new SqlConnection(m_MasterWLConStr))
+                {
+                    using (var adapter = new SqlDataAdapter(strQuery, sqlConnection))
+                    {
+                        adapter.Fill(mytable);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in GettableList -> " + ex.Message);
+            }
+            return mytable;
+        }
+
+        private void SetConnectionString()
+        {
+            string strWorkStation = LocalWorkStationID + @"\" + m_strInstance;
+
+            //Forte Realtime
+            m_ConString = "Data Source ='" + strWorkStation + "'; Database = "
+                + m_strDatabase + "; User id= '" + m_strUserName + "'; Password = '"
+                + m_strPassWrd + "'; connection timeout=30;Persist Security Info=True;";
+
+            //ForteLayer
+            m_WLConStr = "Data Source ='" + strWorkStation + "'; Database = "
+                + m_strWLDatabase + "; User id= '" + m_strUserName + "'; Password = '"
+                + m_strPassWrd + "'; connection timeout=30;Persist Security Info=True;";
+
+            //For Wetlayer Local machine
+            m_MasterWLConStr = "workstation id=" + Environment.MachineName
+                + ";packet size=4096;integrated security=SSPI;data source='"
+                + strWorkStation + "';persist security info=False;initial catalog= " + m_strWLDatabase; // master";
+
+        }
+
+        private void SetSqlParams()
+        {
+            LocalWorkStationID = Environment.MachineName;
+            Settings.Default.LocalHost = LocalWorkStationID;
+            Settings.Default.Save();
+
+
+            if (Settings.Default.Instance != null)
+                m_strInstance = Settings.Default.Instance;
+            else
+                m_strInstance = "SQLEXPRESS";
+
+            if (Settings.Default.UserName != null)
+                m_strUserName = Settings.Default.UserName;
+            else
+                m_strUserName = "forte";
+
+            if (Settings.Default.PassWord != null)
+                m_strPassWrd = Settings.Default.PassWord;
+            else
+                m_strPassWrd = "etrof";
+
+            if (Settings.Default.Database != null)
+                m_strDatabase = Settings.Default.Database;
+            else
+                m_strDatabase = "fortedata";
+
+            if (Settings.Default.WLDatabase != null)
+                m_strWLDatabase = Settings.Default.WLDatabase;
+            else
+                m_strWLDatabase = "ForteLayer";
+
+        }
+    }
+}
---
+++ WpfWLTrend/MainWindow.xaml.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfWLTrend.Properties;
+
+namespace WpfWLTrend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClsSqlConfig _sqlconfig;
+
+        List<string> cmbWLMonthTableList;
+
+        private int iLayerCount;
+        public int DefaultLayersCount = 16;
+        public string totalcount;
+        public int XColumns;
+
+        public List<string> BalerList { get; set; }
+
+        public ObservableCollection<ChartData> _Pos1 { get; set; }
+        public ObservableCollection<ChartData> _Pos2 { get; set; }
+        public ObservableCollection<ChartData> _Pos3 { get; set; }
+        public ObservableCollection<ChartData> _Pos4 { get; set; }
+
+        public double LayerAvg;
+        public double LayerMax;
+        public double LayerMin;
+
+        public double MaximumHeight;
+        public double MinimumHeight;
+
+        public DataTable WetLayerDeltaTable;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           _sqlconfig = new ClsSqlConfig();
+
+            cmbWLMonthTableList = new List<string>();
+
+            cmbWLMonthTableList = _sqlconfig.GetAllWLTables();
+            cmbWLMonthTable.ItemsSource = cmbWLMonthTableList;
+
+            iSampleBales.Minimum = 3;
+            iSampleBales.Maximum = 1000;
+
+            iSampleBales.Text = "200";
+
+            cmbBalerLst.IsEnabled = false;
+            cbBaler.IsChecked = false;
+        }
+
+        private void NumericOnly(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = IsTextNumeric(e.Text);
+        }
+        private static bool IsTextNumeric(string str)
+        {
+            System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex("[^0-9.]+");
+            return reg.IsMatch(str);
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainWindow_Load(object sender, RoutedEventArgs e)
+        {
+
+
+            cmbWLMonthTableList = _sqlconfig.GetAllWLTables();
+
+            if(cmbWLMonthTableList.Count > 0)
+            {
+                cmbWLMonthTable.ItemsSource = cmbWLMonthTableList;
+                cmbWLMonthTable.SelectedIndex = 0;
+                
+                BalerList = GetBalerList(cmbWLMonthTableList[0]);
+                if(BalerList.Count > 0)
+                {
+                    cmbBalerLst.ItemsSource = BalerList;
+                    cmbBalerLst.SelectedIndex = 0;
+                }
+            }
+
+            tbStatus.Text = "Connected to " + Settings.Default.LocalHost;
+           
+        }
+
+        private List<string> GetBalerList(string selectTableValue)
+        {
+            return _sqlconfig.GetUniqueStrItemlist("BalerID", selectTableValue);
+        }
+
+        private void btnApply_click(object sender, RoutedEventArgs e)
+        {
+            GetWLDataGridview(cmbWLMonthTable.Text, iSampleBales.Text, cmbCurrent.Text);
+        }
+
+        private void GetWLDataGridview(object selectedValue, string balecount, string stroccr)
+        {
+            string strOccr = string.Empty;
+            string strAllColumns = "*";
+
+            if (stroccr == "Latest") strOccr = " ORDER BY ReadTime DESC ;";
+            else strOccr = " ORDER BY ReadTime ASC ;";
+
+            string strQuery = "SELECT TOP " + balecount + " " + strAllColumns + " From " + selectedValue + " with (NOLOCK) " + strOccr;
+
+
+            try
+            {
+                DataTable WLDataTable = new DataTable();
+                WLDataTable = _sqlconfig.GetNewWLDataTable(selectedValue, strQuery);
+
+                if (WLDataTable.Rows.Count > 0)
+                {
+                    ProccessData(WLDataTable);
+                    tbStatus.Text = "Each Layers are from the average of " + WLDataTable.Rows.Count.ToString() + " Bales";
+                    xAxis.Title = "Layers Trend of " + WLDataTable.Rows.Count.ToString() + " Bales from " + selectedValue;
+
+                    txttbInfo1.Text = WLDataTable.Rows.Count.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No Record found in  = " + selectedValue);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR in GetWLDataGridview = " + ex.Message);
+            }
+        }
+
+        private void ProccessData(DataTable wLDataTable)
+        {
+            int irows = wLDataTable.Rows.Count;
+            List<Double> ListAvg = new List<double>();
+
+            List<Double> ListDelAvg = new List<double>();
+
+
+            if (wLDataTable.Rows[0]["Layers"] == null)
+                iLayerCount = DefaultLayersCount;
+            else
+                iLayerCount = Convert.ToInt32(wLDataTable.Rows[0]["Layers"].ToString());
+
+            totalcount = wLDataTable.Rows.Count.ToString();
+            XColumns = iLayerCount + 1;
+
+            xAxis.Maximum = XColumns;
+
+            DataTable DtaTable = SetUpDatTable(XColumns);
+
+            //Array of double for each layers.
+            List<double>[] fLayer = new List<double>[iLayerCount];
+
+            SetNewChart();
+
+            try
+            {
+                for (int y = 0; y < iLayerCount; y++)
+                {
+                    fLayer[y] = new List<double>();
+                    for (int i = 0; i < wLDataTable.Rows.Count; i++)
+                    {
+                        if (wLDataTable.Rows[i]["Layer" + (y + 1).ToString()].ToString() != String.Empty)
+                            fLayer[y].Add(ConvToMR(Convert.ToDouble(wLDataTable.Rows[i]["Layer" + (y + 1).ToString()].ToString())));
+                    }
+                }
+                DtaTable.Rows.Add("MR.%");
+                for (int i = 1; i < iLayerCount + 1; i++)
+                {
+                    if (fLayer[i - 1].Count > 1)
+                    {
+                       // _Pos1.Add(new ChartData() { Index = i, Value = fLayer[i - 1].Average() });
+                        ListAvg.Add(fLayer[i - 1].Average());
+                        DtaTable.Rows[0][i] = fLayer[i - 1].Average().ToString("#0.00");
+                    }
+                }
+
+                LayerAvg = Convert.ToDouble(ListAvg.Average().ToString("#0.00"));
+                LayerMax = Convert.ToDouble(ListAvg.Max().ToString("#0.00"));
+                LayerMin = Convert.ToDouble(ListAvg.Min().ToString("#0.00"));
+
+
+
+                DtaTable.Rows.Add("Avg.- MR.%");
+                for (int i = 1; i < iLayerCount + 1; i++)
+                {
+                    if (fLayer[i - 1].Count > 1)
+                    {
+                       
+                        DtaTable.Rows[1][i] = (fLayer[i - 1].Average() - LayerAvg).ToString("#0.00");
+                        ListDelAvg.Add((fLayer[i - 1].Average() - LayerAvg));
+                        _Pos1.Add(new ChartData() { Index = i, Value = (fLayer[i - 1].Average() - LayerAvg) });
+                    }
+                }
+
+                
+                double LayerAvgD = Convert.ToDouble(ListDelAvg.Average().ToString("#0.00"));
+                double LayerMaxD = Convert.ToDouble(ListDelAvg.Max().ToString("#0.00"));
+                double LayerMinD = Convert.ToDouble(ListDelAvg.Min().ToString("#0.00"));
+                
+
+                txtLayerAvg.Text = LayerAvgD.ToString();
+                txtLayerMax.Text = LayerMaxD.ToString();
+                txtLayerMin.Text = LayerMinD.ToString();
+
+                yAxis.Maximum = LayerMaxD + 0.4;
+                yAxis.Minimum = LayerMinD - 0.4;
+
+
+                for (int i = 0; i < iLayerCount + 2; i++)
+                {
+                    _Pos2.Add(new ChartData() { Index = i, Value = LayerAvgD });
+                    _Pos3.Add(new ChartData() { Index = i, Value = LayerMaxD });
+                    _Pos4.Add(new ChartData() { Index = i, Value = LayerMinD });
+                }
+
+                ((LineSeries)ChrtSer1).DataContext = _Pos1;
+                ((LineSeries)ChrtAvg).DataContext = _Pos2;
+                ((LineSeries)ChrtMax).DataContext = _Pos3;
+                ((LineSeries)ChrtMin).DataContext = _Pos4;
+
+                WetLayerDataGrid.DataContext = DtaTable;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR in ProccessData = " + ex.Message);
+            }
+        }
+        private double ConvToMR(double fMoisture)
+        {
+            return (fMoisture / (1 - fMoisture / 100));
+        }
+
+        private void SetNewChart()
+        {
+            if (_Pos1 != null) _Pos1 = null;
+            _Pos1 = new ObservableCollection<ChartData>();
+
+            if (_Pos2 != null) _Pos2 = null;
+            _Pos2 = new ObservableCollection<ChartData>();
+
+            if (_Pos3 != null) _Pos3 = null;
+            _Pos3 = new ObservableCollection<ChartData>();
+
+            if (_Pos4 != null) _Pos4 = null;
+            _Pos4 = new ObservableCollection<ChartData>();
+        }
+
+        private DataTable SetUpDatTable(int LayersColumn)
+        {
+            DataTable NewTable = new DataTable();
+            DataColumn[] DatColumn = new DataColumn[LayersColumn];
+
+            DatColumn[0] = new DataColumn("Type", typeof(string));
+            NewTable.Columns.Add(DatColumn[0]);
+
+            for (int i = 1; i < LayersColumn; i++)
+            {
+                DatColumn[i] = new DataColumn("Layer" + i.ToString(), typeof(double));
+                NewTable.Columns.Add(DatColumn[i]);
+                //Console.WriteLine("DatColumn[i] = " + DatColumn[i] + " i= " + i);
+            }
+            return NewTable;
+        }
+
+        private void cbBaler_checked(object sender, RoutedEventArgs e)
+        {
+            cmbBalerLst.IsEnabled = true;
+        }
+
+        private void cbBaler_unchecked(object sender, RoutedEventArgs e)
+        {
+            cmbBalerLst.IsEnabled = false;
+        }
+    }
+
+    /// <summary>
+    /// Select different colors for Bar Graph
+    /// </summary>
+    public class ChartData //: BindableBase
+    {
+        //private int _Index;
+        public int Index { get; set; }
+        // private double _value;
+        public double Value { get; set; }
+        // private Brush _color;
+        public Brush ChartColor { get; set; }
+      
+    }
+}
